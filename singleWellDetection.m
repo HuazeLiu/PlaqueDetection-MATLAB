4cd449fd34a36d3c0df6e785405991deb4e8ccf1
Huaze(Patrick)  Liu <116246058+HuazeLiu@users.noreply.github.com>
2022-12-07 11:34:53 -0800

    Add files via upload

--- singleWellDetection.m
+++ singleWellDetection.m
@@ -1,10 +1,24 @@
 function [finalCount] = singleWellDetection(inputImage)
-grayscaleimage = rgb2gray(inputImage);
-BW = imbinarize(grayscaleimage,0.60); %0 black, 1 white
-BW = bwareaopen(BW,30);
+% grayscaleimage = rgb2gray(inputImage);
+% imshow(grayscaleimage);
+% BW = imbinarize(grayscaleimage,0.6); %0 black, 1 white
+% BW = bwareaopen(BW,30);
+% 
+% imshow(BW);
+gray_image = rgb2gray(inputImage);
+se = strel('disk', 18)
+background = imopen(gray_image,se);
+imshow(background)
+Image_plaque = gray_image - background;
+imshow(Image_plaque)
+Image_contrast = imadjust(Image_plaque);
+imshow(Image_contrast)
+bw = imbinarize(Image_contrast,0.6);  
+bw = bwareaopen(bw,50);
+imshow(bw)
 
-imshow(BW);
-[B,L] = bwboundaries(BW,'nohole');
+
+[B,L] = bwboundaries(bw,'nohole')
 
 imshow(label2rgb(L, @jet, [.5 .5 .5]))
 hold on
